fa7b3e2468927edd16daa9dfff671e3c5a32d32a
gfixler <gary@garyfixler.com>
2025-02-09 08:34:33 -0500

    Move diacritics into dictionary
    This change will let code later not only use the diacritics, but know
    their names, for use in generating docs, and whatever else I need.

--- fixler-spelling.py
+++ fixler-spelling.py
@@ -1,36 +1,38 @@
-ogonek = "-PBG"
-cedilla = "-RPB"
-ring = "-FRPB"
-hook = "-FRP"
-stroke = "-RB"
+diacritics = {
+    "cedilla": "-RPB",
+    "hook": "-FRP",
+    "ogonek": "-PBG", # "little tail"
+    "ring": "-FRPB",
+    "stroke": "-RB",
+}
 
 connected = [
     {
         "name": "Latin letter A with ogonek",
         "majuscule": ("A", "Ą"),
         "minuscule": ("a", "ą"),
-        "diacritics": [ogonek], #TODO 
+        "diacritics": ["ogonek"], #TODO 
         "link": "https://en.wikipedia.org/wiki/%C4%84",
     },
     {
         "name": "Cyrillic letter A with ogonek"
         "majuscule": ("A", "А̨"),
         "minuscule": ("a", "а̨"),
-        "diacritics": [ogonek],
+        "diacritics": ["ogonek"],
         "link": "https://en.wikipedia.org/wiki/A_with_ogonek_(Cyrillic)",
     },
     {
         "name": "A with cedilla",
         "majuscule": ("A", "Ą"),
         "minuscule": ("a", "Ą"),
-        "diacritics": [cedilla],
+        "diacritics": ["cedilla"],
         "link": "https://en.wikipedia.org/wiki/Cedilla",
     },
     {
         "name": "B with a hook",
         "majuscule": ("B", "Ɓ"),
         "minuscule": ("b", "ɓ"),
-        "diacritics": [hook],
+        "diacritics": ["hook"],
         "link": "https://en.wikipedia.org/wiki/%C6%81",
     },
 }
