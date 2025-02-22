e5615fdeb4c6411c04f39e4a6f31623e107f8974
gfixler <gary@garyfixler.com>
2025-02-22 08:18:45 -0500

    Move readmeTop and generateReadme to  bottom

--- docsgen.py
+++ docsgen.py
@@ -3,40 +3,6 @@ from PIL import Image, ImageDraw, ImageFont
 from fixspell import entries
 
 
-readmeTop = """
-# Fixler Spelling for Plover
-A fingerspelling system for the Plover steno software
-
-## Design Goals
-* allow writing all Latin alphabet letters, upper and lowercase
-* provide system for most precomposed Latin letters with diacritics
-* provide system for many common combining characters
-* allow other symbols, ligatures, etc., on a case-by-case basis
-* absorb some other alphabets (Greek, NATO, Braille, Morse, etc.)
-
-## Diacritic Keys
-The following 6 keys are used to add diacritics.
-![test](http://garyfixler.com/fixlerSpelling/board.png)
-
-## Adding Diacritics
-Add diacritics to letters by stroking a chord from the diacritic keys after stroking a letter chord.
-
-In general, the chord used is meant to visually resemble, the diacritic being added.
-
-NOTE: The chords are pre-built, and don't work with Plover's stroke history. This means you must use the chord specified for each letter in this system, before following up with a diacritic stroke. Currently, if you want to use a different stroke for a letter (e.g. STK instead of STKPW for z), you must change it in the python file. This limitation may be addressed at some point.
-
-## Available Diacritics
-"""
-
-def generateReadme ():
-    print(readmeTop)
-    print("|Pattern|Notes|")
-    print("|-|-|")
-    for diacriticName, combiningMark in combiningMarks.items():
-        print("|" + diacriticName.capitalize() + "| |")
-        img = "![" + diacriticName + "](images/" + diacriticName + ".png)"
-        print("|" + img + "|" + diacriticName + "|")
-
 defaultKeyOpts = {
     "backgroundCol": "#EEEEEE",
 
@@ -125,4 +91,38 @@ def genDiacriticStrokeImage (s="", **kwargs):
     image.paste(G, (x + kw * 2, y + kh))
 
     image.save("test.png", "PNG")
+
+readmeTop = """
+# Fixler Spelling for Plover
+A fingerspelling system for the Plover steno software
+
+## Design Goals
+* allow writing all Latin alphabet letters, upper and lowercase
+* provide system for most precomposed Latin letters with diacritics
+* provide system for many common combining characters
+* allow other symbols, ligatures, etc., on a case-by-case basis
+* absorb some other alphabets (Greek, NATO, Braille, Morse, etc.)
+
+## Diacritic Keys
+The following 6 keys are used to add diacritics.
+![test](http://garyfixler.com/fixlerSpelling/board.png)
+
+## Adding Diacritics
+Add diacritics to letters by stroking a chord from the diacritic keys after stroking a letter chord.
+
+In general, the chord used is meant to visually resemble, the diacritic being added.
+
+NOTE: The chords are pre-built, and don't work with Plover's stroke history. This means you must use the chord specified for each letter in this system, before following up with a diacritic stroke. Currently, if you want to use a different stroke for a letter (e.g. STK instead of STKPW for z), you must change it in the python file. This limitation may be addressed at some point.
+
+## Available Diacritics
+"""
+
+def generateReadme ():
+    print(readmeTop)
+    print("|Pattern|Notes|")
+    print("|-|-|")
+    for diacriticName, combiningMark in combiningMarks.items():
+        print("|" + diacriticName.capitalize() + "| |")
+        img = "![" + diacriticName + "](images/" + diacriticName + ".png)"
+        print("|" + img + "|" + diacriticName + "|")
 
