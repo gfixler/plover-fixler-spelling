9f608c15bde9f5e1880a67c698090a606203ad95
gfixler <gary@garyfixler.com>
2025-03-26 14:20:48 -0500

    Massively rewrite all of imgsgen.py
    I did not develop this in my usually way, through TDD, with careful,
    granular updates. This was a total, seat-of-the-pants free-for-all, so
    I'm committing what I have now, so I can move forward more safely. It's
    able to draw the full keyboard now. I'm about to make it so it can draw
    a subset of the keys, for use in more easily breaking out sections.

--- imgsgen.py
+++ imgsgen.py
@@ -1,172 +1,199 @@
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
+from dataclasses import dataclass
 
 from fixspell import MODIFIERS
 
 
-defaultKeyOpts = {
-    "unit": 57,
+NORTH = (0, -1)
+SOUTH = (0, 1)
+EAST = (1, 0)
+WEST = (-1, 0)
 
-    "faceMargin": 6,
+@dataclass
+class KeyOpts:
+    """
+    Default key options.
+    """
+    unit: int = 57
+    size: tuple = (1, 1)
+    faceMargin: int = 6
+    shiftBy: int = 2
+    shiftDir: tuple = NORTH
+    baseRad: int = 5
+    faceRad: int = 3
+    outlineWidth: int = 1
+    frameMargin: int = 5
+    frameRadius: int = 8
+    typeface: str = "Arial"
+    fontSize: int = 14
 
-    "faceOffsetX": 0,
-    "faceOffsetY": 0,
+@dataclass
+class KeyCols:
+    """
+    Default key colors.
+    """
+    bg = (255, 255, 255, 0)
+    frame: str = "#EEEEEE"
+    border: str = "black"
+    font: str = "black"
+    light: str = "#FCFCFC"
+    dark: str = "#CCCCCC"
+    ridge: str = "#BBBBBB"
 
-    "baseCornerRadius": 5,
-    "faceCornerRadius": 3,
+defaultKeyCols = KeyCols()
 
-    "outlineWidth": 1,
+modifierKeyCols = KeyCols(
+    light = "#46CD34",
+    dark = "#00AA00",
+    ridge = "#009900",
+)
 
-    "frameMargin": 5,
-    "frameRadius": 8,
-}
+tweakKeyCols = KeyCols(
+    light = "#BF69EE",
+    dark = "#A24ED1",
+    ridge = "#9146BC",
+)
 
-defaultKeyCols = {
-    "backgroundCol": "white",
-    "frameCol": "#EEEEEE",
-    "keyOutlineCol": "black",
+def genKey (label, opts=None, cols=None, smoothing=8):
+    # if nothing passed, use defaults
+    opts = opts or KeyOpts()
+    cols = cols or KeyCols()
 
-    "light": "#FCFCFC",
-    "dark": "#CCCCCC",
-    "ridge": "#BBBBBB",
-}
+    # scale up all dimensions if smoothing requires it
+    # drawing large, and scaling at the end, makes a smoother image
+    if smoothing > 1:
+        opts = KeyOpts(**{k: v * smoothing if isinstance(v, int) else v for k, v in vars(opts).items()})
 
-modifierKeyCols = defaultKeyCols | {
-    "light": "#46CD34",
-    "dark": "#00AA00",
-    "ridge": "#009900",
-}
+    unitsWide, unitsHigh = opts.size
 
-tweakKeyCols = defaultKeyCols | {
-    "light": "#BF69EE",
-    "dark": "#A24ED1",
-    "ridge": "#9146BC",
-}
-
-def genKey (label, keyOpts=defaultKeyOpts, keyCols=defaultKeyCols):
-    smoothing = 8
-
-    unit = keyOpts["unit"] * smoothing
-    bgCol = keyCols["frameCol"]
-
-    image = Image.new("RGB", (unit, unit), bgCol)
+    # create an image and a drawing context for it
+    image = Image.new("RGBA", (opts.unit * unitsWide, opts.unit * unitsHigh), cols.bg)
     draw = ImageDraw.Draw(image)
 
-    col = keyCols["dark"]
-    borderCol = keyCols["keyOutlineCol"]
-    stroke = keyOpts["outlineWidth"] * smoothing
-    rad = keyOpts["baseCornerRadius"] * smoothing
-    dims = (0, 0, unit - (stroke / 2), unit - (stroke / 2))
-    opts = {"fill":col, "outline":borderCol, "width":stroke, "radius":rad}
-    draw.rounded_rectangle(dims, **opts)
+    # draw the key base
+    dims = (0, 0, opts.unit * unitsWide - 1, opts.unit * unitsHigh - 1)
+    rectOpts = {
+        "fill": cols.dark,
+        "outline": cols.border,
+        "width": opts.outlineWidth,
+        "radius": opts.baseRad
+    }
+    draw.rounded_rectangle(dims, **rectOpts)
 
-    faceMargin = keyOpts["faceMargin"] * smoothing
-    xOffset = keyOpts["faceOffsetX"]
-    yOffset = keyOpts["faceOffsetY"]
+    # set up top face shift, if any
+    shiftX, shiftY = opts.shiftDir
+    xShift = shiftX * opts.shiftBy
+    yShift = shiftY * opts.shiftBy
 
-    xOffset *= smoothing
-    yOffset *= smoothing
-    x = faceMargin + xOffset
-    y = faceMargin + yOffset
-    w = unit - faceMargin + xOffset
-    h = unit - faceMargin + yOffset
-    col = keyCols["light"]
-    ridgeCol = keyCols["ridge"]
-    stroke = keyOpts["outlineWidth"] * smoothing
-    rad = keyOpts["faceCornerRadius"] * smoothing
-    dims = (x, y, w - (stroke / 2), h - (stroke / 2))
-    opts = {"fill":col, "outline":ridgeCol, "width":stroke, "radius":rad}
-    draw.rounded_rectangle(dims, **opts)
+    # draw the key face
+    x = opts.faceMargin + xShift
+    y = opts.faceMargin + yShift
+    w = opts.unit * unitsWide - opts.faceMargin + xShift
+    h = opts.unit * unitsHigh - opts.faceMargin + yShift
+    dims = (x, y, w - 1, h - 1)
+    rectOpts = {
+        "fill": cols.light,
+        "outline": cols.ridge,
+        "width": opts.outlineWidth,
+        "radius": opts.faceRad
+    }
+    draw.rounded_rectangle(dims, **rectOpts)
 
-    font = ImageFont.truetype('Arial', 14 * smoothing)
+    # add the label to the key
+    font = ImageFont.truetype(opts.typeface, opts.fontSize)
     _, _, tw, th = draw.textbbox((0, 0), label, font)
-    dims = ((unit-tw) / 2 + xOffset, (unit-th) / 2 + yOffset)
+    dims = ((opts.unit * unitsWide - tw) / 2 + xShift, (opts.unit * unitsHigh - th) / 2 + yShift)
     draw.text(dims, label, font=font, fill="black")
 
-    downRes = image.resize((image.width // smoothing, image.height // smoothing), resample=Image.LANCZOS)
+    if smoothing > 1:
+        newWidth = image.width // smoothing
+        newHeight = image.height // smoothing
+        final = image.resize((newWidth, newHeight), resample=Image.LANCZOS)
+    else:
+        final = image
 
-    return downRes
+    return final
 
+# def genDiacriticStrokeImage (stroke="", keyOpts=defaultKeyOpts, keyCols=defaultKeyCols):
+#     pressed = lambda k: modifierKeyCols if k in stroke else defaultKeyCols
 
-def genDiacriticStrokeImage (stroke="", keyOpts=defaultKeyOpts, keyCols=defaultKeyCols):
-    pressed = lambda k: modifierKeyCols if k in stroke else defaultKeyCols
+#     north = {"faceOffsetY": 2}
+#     south = {"faceOffsetY": -2}
+#     east = {"faceOffsetX": -2}
+#     west = {"faceOffsetX": 2}
 
-    north = {"faceOffsetY": 2}
-    south = {"faceOffsetY": -2}
-    east = {"faceOffsetX": -2}
-    west = {"faceOffsetX": 2}
+#     F = genKey("F", keyOpts | north, pressed("F"))
+#     R = genKey("R", keyOpts | south, pressed("R"))
+#     P = genKey("P", keyOpts | north, pressed("P"))
+#     B = genKey("B", keyOpts | south, pressed("B"))
+#     L = genKey("L", keyOpts | north, pressed("L"))
+#     G = genKey("G", keyOpts | south, pressed("G"))
 
-    F = genKey("F", keyOpts | north, pressed("F"))
-    R = genKey("R", keyOpts | south, pressed("R"))
-    P = genKey("P", keyOpts | north, pressed("P"))
-    B = genKey("B", keyOpts | south, pressed("B"))
-    L = genKey("L", keyOpts | north, pressed("L"))
-    G = genKey("G", keyOpts | south, pressed("G"))
+#     kw = F.width
+#     kh = F.height
 
-    kw = F.width
-    kh = F.height
+#     margin = 8
+#     w = kw * 3 + margin * 2
+#     h = kh * 2 + margin * 2
+#     x = margin
+#     y = margin
 
-    margin = 8
-    w = kw * 3 + margin * 2
-    h = kh * 2 + margin * 2
-    x = margin
-    y = margin
+#     bgCol = keyCols["bg"]
+#     image = Image.new("RGBA", (w, h), bgCol)
+#     draw = ImageDraw.Draw(image)
 
-    bgCol = keyCols["backgroundCol"]
-    image = Image.new("RGB", (w, h), bgCol)
-    draw = ImageDraw.Draw(image)
+#     col = keyCols["frame"]
+#     rad = opts.frameRadius
+#     dims = (0, 0, image.width, image.height)
+#     opts = {"fill":col, "radius":rad}
+#     draw.rounded_rectangle(dims, **opts)
 
-    col = keyCols["frameCol"]
-    rad = keyOpts["frameRadius"]
-    dims = (0, 0, image.width, image.height)
-    opts = {"fill":col, "radius":rad}
-    draw.rounded_rectangle(dims, **opts)
+#     image.paste(F, (x, y))
+#     image.paste(R, (x, y + kw))
+#     image.paste(P, (x + kw, y))
+#     image.paste(B, (x + kw, y + kh))
+#     image.paste(L, (x + kw * 2, y))
+#     image.paste(G, (x + kw * 2, y + kh))
 
-    image.paste(F, (x, y))
-    image.paste(R, (x, y + kw))
-    image.paste(P, (x + kw, y))
-    image.paste(B, (x + kw, y + kh))
-    image.paste(L, (x + kw * 2, y))
-    image.paste(G, (x + kw * 2, y + kh))
-
-    return image
+#     return image
 
 def genDiacriticImages ():
     for modName, modData in MODIFIERS.items():
         image = genDiacriticStrokeImage(modData["outline"])
         image.save("images/" + modName + ".png", "PNG")
 
-def genTweakStrokeImage (stroke="", keyOpts=defaultKeyOpts, keyCols=defaultKeyCols):
-    pressed = lambda k: tweakKeyCols if k in stroke else defaultKeyCols
+# def genTweakStrokeImage (stroke="", keyOpts=defaultKeyOpts, keyCols=defaultKeyCols):
+#     pressed = lambda k: tweakKeyCols if k in stroke else defaultKeyCols
 
-    east = {"faceOffsetX": -2}
-    west = {"faceOffsetX": 2}
+#     east = {"faceOffsetX": -2}
+#     west = {"faceOffsetX": 2}
 
-    E = genKey("E", keyOpts | west, pressed("E"))
-    U = genKey("U", keyOpts | east, pressed("U"))
+#     E = genKey("E", keyOpts | west, pressed("E"))
+#     U = genKey("U", keyOpts | east, pressed("U"))
 
-    kw = E.width
-    kh = E.height
+#     kw = E.width
+#     kh = E.height
 
-    margin = 8
-    w = kw * 2 + margin * 2
-    h = kh * 1 + margin * 2
-    x = margin
-    y = margin
+#     margin = 8
+#     w = kw * 2 + margin * 2
+#     h = kh * 1 + margin * 2
+#     x = margin
+#     y = margin
 
-    bgCol = keyCols["backgroundCol"]
-    image = Image.new("RGB", (w, h), bgCol)
-    draw = ImageDraw.Draw(image)
+#     bgCol = keyCols["bg"]
+#     image = Image.new("RGBA", (w, h), bgCol)
+#     draw = ImageDraw.Draw(image)
 
-    rad = keyOpts["frameRadius"]
-    col = keyCols["frameCol"]
-    dims = (0, 0, image.width, image.height)
-    opts = {"fill":col, "radius":rad}
-    draw.rounded_rectangle(dims, **opts)
+#     rad = keyOpts["frameRadius"]
+#     col = keyCols["frame"]
+#     dims = (0, 0, image.width, image.height)
+#     opts = {"fill":col, "radius":rad}
+#     draw.rounded_rectangle(dims, **opts)
 
-    image.paste(E, (x, y))
-    image.paste(U, (x + kw, y))
+#     image.paste(E, (x, y))
+#     image.paste(U, (x + kw, y))
 
-    return image
+#     return image
 
 def genTweakImages ():
     genTweakStrokeImage("").save("images/EU_up.png", "PNG")
@@ -177,4 +204,75 @@ def genTweakImages ():
 if __name__ == "__main__":
     genDiacriticImages()
     genTweakImages()
+
+UNIT = (1, 1)
+
+@dataclass
+class Key:
+    label: str = ""
+    pos: tuple = (0, 0)
+    shift: tuple = NORTH
+    size: tuple = UNIT
+    cols: dict = KeyCols()
+
+def buildKeyboard (kbdKeys):
+    return [Key(*k) for k in kbdKeys]
+
+def renderKeyboard (keys):
+    minX = minY = float('inf')
+    maxX = maxY = float('-inf')
+
+    renders = []
+    for key in keys:
+        x, y = key.pos
+
+        options = {
+            "shiftDir": key.shift,
+            "size": key.size,
+        }
+        opts = KeyOpts(**options)
+        keyImg = genKey(key.label, opts, key.cols)
+        imgW, imgH = keyImg.size
+
+        keyX = x * imgW
+        keyY = y * imgH
+
+        minX = min(minX, keyX)
+        minY = min(minY, keyY)
+        maxX = max(maxX, keyX + imgW)
+        maxY = max(maxY, keyY + imgH)
+
+        renders.append((keyImg, keyX, keyY))
+
+    image = Image.new("RGBA", (maxX, maxY), (255, 255, 255, 0))
+
+    for keyImg, x, y in renders:
+        image.paste(keyImg, (x, y))
+
+    return image
+
+stenoKeyboard = [
+    ("S", (0,0),  EAST, (1,2), defaultKeyCols),
+    ("T", (1,0), SOUTH,  UNIT, defaultKeyCols),
+    ("K", (1,1), NORTH,  UNIT, defaultKeyCols),
+    ("P", (2,0), SOUTH,  UNIT, defaultKeyCols),
+    ("W", (2,1), NORTH,  UNIT, defaultKeyCols),
+    ("H", (3,0), SOUTH,  UNIT, defaultKeyCols),
+    ("R", (3,1), NORTH,  UNIT, defaultKeyCols),
+    ("A", (2,2),  EAST,  UNIT, defaultKeyCols),
+    ("O", (3,2),  WEST,  UNIT, defaultKeyCols),
+    ("*", (4,0), (0,0), (1,2), defaultKeyCols),
+    ("E", (5,2),  EAST,  UNIT, tweakKeyCols),
+    ("U", (6,2),  WEST,  UNIT, tweakKeyCols),
+    ("F", (5,0), SOUTH,  UNIT, modifierKeyCols),
+    ("R", (5,1), NORTH,  UNIT, modifierKeyCols),
+    ("P", (6,0), SOUTH,  UNIT, modifierKeyCols),
+    ("B", (6,1), NORTH,  UNIT, modifierKeyCols),
+    ("L", (7,0), SOUTH,  UNIT, modifierKeyCols),
+    ("G", (7,1), NORTH,  UNIT, modifierKeyCols),
+    ("T", (8,0), SOUTH,  UNIT, defaultKeyCols),
+    ("S", (8,1), NORTH,  UNIT, defaultKeyCols),
+    ("D", (9,0), SOUTH,  UNIT, defaultKeyCols),
+    ("Z", (9,1), NORTH,  UNIT, defaultKeyCols),
+]
 
